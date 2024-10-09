172f617b981fcd5d775b8aa2fe2f335b67e66933
nguyen_phuc <nguyenphuc02042004@gmail.com>
2024-10-09 10:47:40 +0700

    country code picker, custom, fragment folder

--- app/src/main/java/vn/md18/fsquareapplication/data/network/AppAPi.kt
+++ app/src/main/java/vn/md18/fsquareapplication/data/network/AppAPi.kt
@@ -5,4 +5,7 @@ object AppAPi {
     const val AUTH_SIGNUP = "/auth/customer/v1/registrations"
     const val AUTH_LOGIN = "/auth/customer/v1/authentications"
     const val AUTH_VERIFY_OTP = "/auth/customer/v1/verifications"
+
+
+    const val GET_COUNTRY_NAME = "https://restcountries.com/v3.1/all?fields=name"
 }
