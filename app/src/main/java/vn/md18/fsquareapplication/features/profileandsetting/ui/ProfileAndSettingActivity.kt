e73c48f8d4ebc6c487784b828c464d106e76e72b
nguyen_phuc <nguyenphuc02042004@gmail.com>
2024-10-09 15:59:59 +0700

    country code picker, custom, fragment folder

--- app/src/main/java/vn/md18/fsquareapplication/features/profileandsetting/ui/ProfileAndSettingActivity.kt
+++ app/src/main/java/vn/md18/fsquareapplication/features/profileandsetting/ui/ProfileAndSettingActivity.kt
@@ -1,21 +1,39 @@
 package vn.md18.fsquareapplication.features.profileandsetting.ui
 
 import android.os.Bundle
+import android.view.LayoutInflater
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import dagger.hilt.android.AndroidEntryPoint
 import vn.md18.fsquareapplication.R
+import vn.md18.fsquareapplication.core.base.BaseActivity
+import vn.md18.fsquareapplication.databinding.ActivityAuthBinding
+import vn.md18.fsquareapplication.databinding.ActivityProfileAndSettingBinding
+import vn.md18.fsquareapplication.features.auth.viewmodel.AuthViewModel
+import vn.md18.fsquareapplication.features.profileandsetting.viewmodel.ProfileViewModel
 
-class ProfileAndSettingActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_profile_and_setting)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+@AndroidEntryPoint
+class ProfileAndSettingActivity : BaseActivity<ActivityProfileAndSettingBinding, ProfileViewModel>() {
+
+    override val viewModel: ProfileViewModel by viewModels()
+    private lateinit var navController: NavController
+    override fun inflateLayout(layoutInflater: LayoutInflater): ActivityProfileAndSettingBinding {
+        return ActivityProfileAndSettingBinding.inflate(layoutInflater)
     }
+
+    override fun onViewLoaded() {
+        val navHostFragment = supportFragmentManager
+            .findFragmentById(R.id.profile_nav_host_fragment) as NavHostFragment
+        navController = navHostFragment.navController
+    }
+
+    override fun addViewListener() {
+
+    }
+
 }
--- app/src/main/java/vn/md18/fsquareapplication/features/profileandsetting/ui/fragment/ProfileFragment.kt
+++ app/src/main/java/vn/md18/fsquareapplication/features/profileandsetting/ui/fragment/ProfileFragment.kt
@@ -1,11 +1,41 @@
 package vn.md18.fsquareapplication.features.profileandsetting.ui.fragment
-
+import android.content.Context
+import android.content.SharedPreferences
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.widget.Toast
+import androidx.fragment.app.viewModels
+import com.hbb20.CountryPickerView
+import com.hbb20.countrypicker.models.CPCountry
+import dagger.hilt.android.AndroidEntryPoint
+import vn.md18.fsquareapplication.R
+import vn.md18.fsquareapplication.core.base.BaseFragment
+import vn.md18.fsquareapplication.databinding.FragmentProfileBinding
+import vn.md18.fsquareapplication.features.profileandsetting.viewmodel.ProfileViewModel
+import vn.md18.fsquareapplication.utils.Constant
+import vn.md18.fsquareapplication.utils.extensions.showCustomToast
 
-class ProfileFragment : Fragment() {
+@AndroidEntryPoint
+class ProfileFragment : BaseFragment<FragmentProfileBinding, ProfileViewModel>() {
 
+    override val viewModel: ProfileViewModel by viewModels()
+
+
+    override fun inflateLayout(layoutInflater: LayoutInflater): FragmentProfileBinding =
+        FragmentProfileBinding.inflate(layoutInflater)
+
+    override fun getTagFragment(): String {
+        return "ProfileFragment"
+    }
+
+    override fun onViewLoaded() {
+
+    }
+
+    override fun addViewListener() {
+
+    }
+
+    override fun addDataObserver() {
+    }
 }
