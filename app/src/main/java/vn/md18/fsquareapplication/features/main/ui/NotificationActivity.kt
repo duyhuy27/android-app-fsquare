88190a2a63fe8ce0c0783c422562dc69bf1ccded
nguye <nguyenphuc02042004@gmail.com>
2024-10-21 09:43:31 +0700

    create payment navigation

---
+++ app/src/main/java/vn/md18/fsquareapplication/features/main/ui/NotificationActivity.kt
@@ -0,0 +1,21 @@
+package vn.md18.fsquareapplication.features.main.ui
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import vn.md18.fsquareapplication.R
+
+class NotificationActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_notification)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
